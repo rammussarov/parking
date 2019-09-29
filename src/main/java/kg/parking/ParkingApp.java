be83b2fed53a8cbdcb9728d3fa1986adaa761528
rammussarov <rammussarov@gmail.com>
2019-09-29 12:24:21 +0600

    added maven dependencies

---
+++ src/main/java/kg/parking/ParkingApp.java
@@ -0,0 +1,12 @@
+package kg.parking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParkingApp.class, args);
+    }
+}

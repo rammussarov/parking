42aca052a7299550e745ab82ceb941007faf87de
rammussarov <rammussarov@gmail.com>
2019-09-29 19:51:09 +0600

    test data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,30 @@
+INSERT  INTO parking_place (id, position, is_taken, is_booked, parking_fee) VALUES
+(parking_ticket_seq.nextval, 'A 1', FALSE, FALSE, 10.0);
+
+INSERT  INTO parking_place (id, position, is_taken, is_booked, parking_fee) VALUES
+(parking_ticket_seq.nextval, 'A 2', FALSE, FALSE, 12.0);
+
+INSERT  INTO parking_place (id, position, is_taken, is_booked, parking_fee) VALUES
+(parking_ticket_seq.nextval, 'A 3', FALSE, FALSE, 12.0);
+
+INSERT  INTO parking_place (id, position, is_taken, is_booked, parking_fee) VALUES
+(parking_ticket_seq.nextval, 'A 4', FALSE, FALSE, 15.0);
+
+INSERT  INTO parking_place (id, position, is_taken, is_booked, parking_fee) VALUES
+(parking_ticket_seq.nextval, 'A 5', FALSE, FALSE, 10.0);
+
+INSERT  INTO parking_place (id, position, is_taken, is_booked, parking_fee) VALUES
+(parking_ticket_seq.nextval, 'B 1', FALSE, FALSE, 12.0);
+
+INSERT  INTO parking_place (id, position, is_taken, is_booked, parking_fee) VALUES
+(parking_ticket_seq.nextval, 'B 2', FALSE, FALSE, 12.0);
+
+INSERT  INTO parking_place (id, position, is_taken, is_booked, parking_fee) VALUES
+(parking_ticket_seq.nextval, 'B 3', FALSE, FALSE, 15.0);
+
+INSERT  INTO parking_place (id, position, is_taken, is_booked, parking_fee) VALUES
+(parking_ticket_seq.nextval, 'B 4', FALSE, FALSE, 10.0);
+
+INSERT  INTO parking_place (id, position, is_taken, is_booked, parking_fee) VALUES
+(parking_ticket_seq.nextval, 'B 5', FALSE, FALSE, 15.0);
+
